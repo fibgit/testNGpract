b7518c215be7ce4ae46c68520fc2c4f053aa0a30
bomafiber <bomafiber@yahoo.co.uk>
2019-07-29 20:25:45 +0100

    Modifications for SearchforDomain code

--- src/main/java/TrainingKit/TestNGtests.java
+++ src/main/java/TrainingKit/TestNGtests.java
@@ -1,6 +1,7 @@
 package TrainingKit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,11 +11,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 
 public class TestNGtests {
     private WebDriver driver = new ChromeDriver();
+   private WebDriverWait wait;
 
 
 
@@ -23,6 +26,7 @@ public class TestNGtests {
     public void setUp(){
         driver.get("https://www.register.com");
         driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
     }
 
     @AfterMethod(alwaysRun = true)
@@ -56,25 +60,29 @@ public class TestNGtests {
         searchField.clear();
         searchField.sendKeys("biochemical.com");
         Thread.sleep(2000);
+        searchField.sendKeys(Keys.ENTER);
 
-        WebElement searchButton = driver.findElement(By.id("domainSearch"));
-        searchButton.click();
 
-        WebDriverWait toWait = new WebDriverWait(driver,30);
 
-        WebElement secondSearchField = toWait.until(presenceOfElementLocated(By.id("SearchedDomain")));
-        WebElement secondDomainButton = toWait.until(presenceOfElementLocated(By.xpath("//input[@value='Search Domains']")));
+        Thread.sleep(30);
+        WebElement secondSearchField = wait.until(elementToBeClickable(By.id("SearchedDomain")));
+        wait(10);
+        secondSearchField.clear();
+        secondSearchField.sendKeys("Jonobo.com");
 
-        WebElement notFoundDomain = toWait.until(presenceOfElementLocated(By.xpath("//span[@class='not-avail']")));
-        WebElement isAvailableDomain = toWait.until(presenceOfElementLocated(By.xpath("//h1[@class='congrats']")));
-        WebElement addToCartButton = toWait.until(presenceOfElementLocated(By.xpath("//input[@class='button btn-standard addDomainToCart']")));
+        WebElement secondDomainButton = wait.until(presenceOfElementLocated(By.xpath("//input[@value='Search Domains']")));
+        secondDomainButton.click();
 
-        if (notFoundDomain.isDisplayed()) {
-            secondSearchField.sendKeys("biochemistry.solutions");
-            secondDomainButton.click();
-        } else if(isAvailableDomain.isDisplayed()) {
-            addToCartButton.click();
-        }
+        WebElement notFoundDomain = wait.until(presenceOfElementLocated(By.xpath("//span[@class='not-avail']")));
+        WebElement isAvailableDomain = wait.until(presenceOfElementLocated(By.xpath("//h1[@class='congrats']")));
+        WebElement addToCartButton = wait.until(presenceOfElementLocated(By.xpath("//input[@class='button btn-standard addDomainToCart']")));
+
+//        if (notFoundDomain.isDisplayed()) {
+//            secondSearchField.sendKeys("biochemistry.solutions");
+//            secondDomainButton.click();
+//        } else if(isAvailableDomain.isDisplayed()) {
+//            addToCartButton.click();
+//        }
     /*
         Assert.assertEquals(driver.findElement(By.id("biochemical.online-action")).isDisplayed());
             domainAvailableButton.click();
